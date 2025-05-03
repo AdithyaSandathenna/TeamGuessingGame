5a9a5735bffaeb818d9d2481ca5edd7fe085b7d2
Dilshan74 <dilshan06akalanka2003@gmail.com>
2025-05-03 19:22:41 +0530

    Added while loop to replay the game

--- guessing_game.py
+++ guessing_game.py
@@ -1,16 +1,15 @@
 import random 
-number = random.randint(1, 100) 
-print("Guess a number between 1 and 10") 
-guess = int(input())
-
-if guess < number:
-    print("Too low!")
-elif guess > number:
-    print("Too high!")
-
-if guess == number: 
-print("You win!") 
-else: 
-print(f"Wrong! The number was {number}") 
+while True:
+    number = random.randint(1, 100) 
+    print("Guess a number between 1 and 10") 
+    guess = int(input())
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    if guess == number: 
+        print("You win!") 
+    else: 
+        print(f"Wrong! The number was {number}") 
 
 
